d5a0f7dfa34c048c66087236b2dcb0115f70e0c1
Emilio Blacksmith <71019624+OcelotDevelop@users.noreply.github.com>
2021-09-30 22:15:54 -0500

    Initial commit

--- U.2D Platformer Controller/Assets/_Project/Scripts/CharacterMovement.cs
+++ U.2D Platformer Controller/Assets/_Project/Scripts/CharacterMovement.cs
@@ -1,47 +1,52 @@
- using UnityEngine;
+using UnityEngine;
 
 namespace OcelotDev
 {
     public class CharacterMovement : MonoBehaviour
     {
+        [Header("Movement")]
+        public float movementSpeed = 12f;
         public float horizontalMovement;
-        public float movementSpeed = 2f;
-        public bool onGround = false;
-        public float jumpForce = 5f;
 
-        [Header("Raycasting Ground")]
+        [Header("Gravity")]
+        public bool onGround = false;
+        public float JumpForce = 20f;
+        private Rigidbody2D rgb;
+
+        [Header("Raycast")]
         public float groundLength = 0.6f;
         public Vector3 colliderOffset;
         public LayerMask groundLayer;
 
-        [Header("The Motherfucker")]
-        private Rigidbody2D rgb;
+        private void Start()
+        {
+            rgb = GetComponent<Rigidbody2D>();
+        }
 
-        private void Start() => rgb = GetComponent<Rigidbody2D>();
+        private void FixedUpdate()
+        {
+            horizontalMovement = Input.GetAxisRaw("Horizontal");
+        }
 
-        public void FixedUpdate() => horizontalMovement = Input.GetAxisRaw("Horizontal"); 
-
-        public void LateUpdate()
+        private void LateUpdate()
         {
             transform.Translate(new Vector3(horizontalMovement, 0f, 0f) * movementSpeed * Time.deltaTime);
-            //rgb.MovePosition(transform.position + new Vector3(horizontalMovement, verticalMovement, 0f));
 
-            onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, groundLayer) 
-            || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, groundLayer);
+            onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, groundLayer)
+                || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, groundLayer);
 
             if(Input.GetKeyDown(KeyCode.Space) && onGround)
             {
                 Jump();
-            }      
+            }
         }
 
         void Jump()
         {
-            rgb.velocity = new Vector2(rgb.velocity.x, 0);
-            rgb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rgb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
         }
 
-        private void OnDrawGizmos() 
+        private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
             Gizmos.DrawLine(transform.position + colliderOffset, transform.position + colliderOffset + Vector3.down * groundLength);
